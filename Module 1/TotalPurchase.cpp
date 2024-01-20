fd4fb6fc0cb45b4ceff70476537364609b90210b
Sharri'a Sneed <frostellachery26@gmail.com>
2024-01-20 14:30:30 -0600

    Total Purchase (Start)
    Beginning the graded assignment total purchase for module 1

---
+++ Module 1/TotalPurchase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int item1, item2, item3, item4, item5;
+    
+    //Asking the users to input values and save them to their respective variables
+    cout << "Enter the price of your first item: $";
+    cin >> item1;
+    cout << endl << "Enter the price of your second item: $";
+    cin >> item2;
+    cout << endl << "Enter the price of your third item: $";
+    cin >> item3;
+    cout << endl << "Enter the price of your fourth item: $";
+    cin >> item4;
+    cout << endl << "Enter the price of your fifth item: $";
+    cin >> item5;
+
+    return 0;
+}
